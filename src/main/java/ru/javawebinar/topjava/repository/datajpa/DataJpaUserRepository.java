927d58c9193bc053a46d39715e81da5d99f1cf3d
seregatttt <seregatttt@mail.ru>
2020-04-01 21:47:55 +0300

    HW05.1 (part1-2.3)

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepository.java
@@ -39,4 +39,5 @@ public class DataJpaUserRepository implements UserRepository {
     public List<User> getAll() {
         return crudRepository.findAll(SORT_NAME_EMAIL);
     }
+
 }
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -34,7 +34,9 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
+//@ActiveProfiles({"postgres","jdbc"})
+//@ActiveProfiles({"postgres","jpa"})
+@ActiveProfiles({"postgres",  "datajpa"})
 public class MealServiceTest {
     private static final Logger log = getLogger("result");
 
